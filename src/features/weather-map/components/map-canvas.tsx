5913721f4f00054036e8f68f4cc1917b8aec2875
trieu04 <115911417+trieu04@users.noreply.github.com>
2025-09-25 07:16:29 +0700

    feat(weather-map): update map layer to Google Maps and enhance precipitation and level pages with slider marks

--- src/features/weather-map/components/map-canvas.tsx
+++ src/features/weather-map/components/map-canvas.tsx
@@ -4,8 +4,9 @@ import "leaflet/dist/leaflet.css";
 export function MapCanvas() {
   return (
     <TileLayer
-      url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-      attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+      url="http://{s}.google.com/vt/lyrs=m&x={x}&y={y}&z={z}"
+      attribution='&copy; <a href="https://www.google.com/maps">Google Maps</a>'
+      subdomains={["mt0", "mt1", "mt2", "mt3"]}
     />
   );
 }
